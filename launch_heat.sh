f7ab089863a4803bf0f84b634bf285f5616afc2b
Alan Kennedy-Asser <alan.kennedy@bristol.ac.uk>
2022-04-04 14:27:07 +0100

    Updating unzipping

--- launch_heat.sh
+++ launch_heat.sh
@@ -2,9 +2,9 @@
 
 echo "Copying and unzipping datasets to working directory"
 cp /data/inputs/PreProcessedData/PreProcessedData.tar.gz /code/PreProcessedData.tar.gz
-tar -xfz /code/PreProcessedData.tar.gz -C /code/PreProcessedData/
+tar -xf /code/PreProcessedData.tar.gz -C /code/PreProcessedData/
 cp /data/inputs/UKCP18dir/UKCP18_subset.tar.gz /code/UKCP18_subset.tar.gz
-tar -xfz /code/UKCP18_subset.tar.gz -C /code/UKCP18dir/
+tar -xf /code/UKCP18_subset.tar.gz -C /code/UKCP18dir/
 
 
 # echo "Running containerised model"
