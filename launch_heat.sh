32697458b2bb792f2950949440e04ae55e27b4e8
Alan Kennedy-Asser <alan.kennedy@bristol.ac.uk>
2022-04-04 14:15:02 +0100

    Changing zip to tar.gz

--- launch_heat.sh
+++ launch_heat.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 echo "Copying and unzipping datasets to working directory"
-cp /data/inputs/PreProcessedData.zip /code/PreProcessedData.zip
-unzip /code/PreProcessedData.zip -d /code/PreProcessedData/
-cp /data/inputs/UKCP18_subset.zip /code/UKCP18_subset.zip
-unzip /code/UKCP18_subset.zip -d /code/UKCP18dir/
+cp /data/inputs/PreProcessedData/PreProcessedData.tar.gz /code/PreProcessedData.tar.gz
+tar -xfz /code/PreProcessedData.tar.gz -C /code/PreProcessedData/
+cp /data/inputs/UKCP18dir/UKCP18_subset.tar.gz /code/UKCP18_subset.tar.gz
+tar -xfz /code/UKCP18_subset.tar.gz -C /code/UKCP18dir/
 
-# echo "Running Hello World Model"
+
+# echo "Running containerised model"
 /usr/bin/mlrtapp/HEAT
-
-# echo "Copying outputs to DAFNI outputs directory"
-# cp /code/output.txt /data/outputs/output.txt
-
-# echo "Printing contents of DAFNI outputs directory"
-# ls -la /data/outputsd
