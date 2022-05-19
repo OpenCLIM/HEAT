8e2998a7307f0301d0a88cb9b03206678b11b2d5
Alan Kennedy-Asser <alan.kennedy@bristol.ac.uk>
2022-05-19 11:47:51 +0100

    adjusting launch_file

--- launch_heat.sh
+++ launch_heat.sh
@@ -5,7 +5,7 @@ cp /data/inputs/PreProcessedData/PreProcessedData.tar.gz /code/PreProcessedData.
 tar -xf /code/PreProcessedData.tar.gz -C /code/PreProcessedData/
 cp /data/inputs/UKCP18dir/UKCP18_subset.tar.gz /code/UKCP18_subset.tar.gz
 tar -xf /code/UKCP18_subset.tar.gz -C /code/UKCP18dir/
-cp /data/inputs/input_file/* /code/inputs.m
+cp /data/inputs/input_file/* /code/
 
 
 
